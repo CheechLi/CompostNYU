4724bd179a3fcf30e61f1c436e373999ce497951
Eugene-S-Hwang <egswhwang@gmail.com>
2025-02-08 15:51:49 -0500

    streamlit

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
-print("Hello")
-print("Hello Eugene Cheech and Glebas")
+green_amt = 0
+brown_amt = 0
+
+def addGreen():
+    st.write("Green added")
+
+def main():
+    st.set_page_config(page_title="Composted", layout="centered")
+    st.button("Greens", on_click=addGreen)
+
+if __name__ == "__main__":
+    main()
