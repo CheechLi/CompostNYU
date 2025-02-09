a54a90700f368e7189e9b5664f47260d25f97b90
Eugene-S-Hwang <egswhwang@gmail.com>
2025-02-09 00:23:15 -0500

    small edit

--- Home.py
+++ Home.py
@@ -5,7 +5,8 @@ def main():
     st.markdown("<h1 style='text-align: center; font-size: 150px; color: #90EE90'>CompostED</h1>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center; font-size: 50px;'>Composting is the cornerstone of food sustainability</h2>", unsafe_allow_html=True)
     st.markdown("<h3 style='text-align: center;'>Explore the benefits of composting with our composting simulator and chatbot!</h3>", unsafe_allow_html=True)
-    st.page_link("pages/2_Composting_Chatbot.py", label="Simulator", icon="➡️>")
+    st.page_link("pages/2_Composting_Chatbot.py", label="Chatbot", icon="➡️")
+    st.page_link("pages/3_Composting_Simulator.py", label="Simulator", icon="➡️")
 
 if __name__ == "__main__":
     main()
